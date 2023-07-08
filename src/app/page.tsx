3acacaf471c44bea96e90ff6c1624845f48b5bf3
hakeem-jc <hakeem_clarke14@yahoo.com>
2023-07-07 22:37:53 -0500

    Start post component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import styles from './page.module.scss' 
+import styles from './page.module.scss';
+import Post from '@/components/Post/Post';
 
 export default function App() {
   return (
     <main className={styles.app}>
-     
+      <h1>@</h1>
+      <Post />
     </main>
   )
 }
