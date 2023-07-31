c8320473965f483ad5bdaaedf7b9d34c90df753f
hakeem-jc <hakeem_clarke14@yahoo.com>
2023-07-30 19:07:18 -0500

    Refactor to add other pages later

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 import './styles/globals.scss'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import styles from './page.module.scss';
+import Image from 'next/image';
+import home from '@/assets/icons/home.svg';
+import search from '@/assets/icons/search.svg';
+import edit from '@/assets/icons/edit.svg';
+import favorite from '@/assets/icons/favorite.svg';
+import person from '@/assets/icons/person.svg';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +24,33 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <main className={styles.app}>
+          <section className={styles.content}>
+            <h1>@</h1>
+            {children}
+          </section>
+        <nav className={styles.menu}>
+          <ul>
+            <li>
+              <Image src={home} className={styles.icon} alt='Home' />
+            </li>
+            <li>
+              <Image src={search} className={styles.icon} alt='Search' />
+            </li>
+            <li>
+              <Image src={edit} className={styles.icon} alt='Post' />
+            </li>
+            <li>
+              <Image src={favorite} className={styles.icon} alt='Likes' />
+            </li>
+            <li>
+              <Image src={person} className={styles.icon} alt='Profile' />
+            </li>
+          </ul>
+        </nav>
+      </main>
+        </body>
     </html>
   )
 }
