9f2231c58a9595d06312281923e436c4be2c9a10
hakeem-jc <hakeem_clarke14@yahoo.com>
2023-08-04 06:44:03 -0500

    Add data fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import styles from "./page.module.scss";
-import PostFeed from "@/pages/PostFeed/PostFeed";
+import PostFeed from "@/pages/PostFeed";
 
-export default function App() {
+export default async function App() {
   return (
     <main className={styles.app}>
       <section className={styles.content}>
